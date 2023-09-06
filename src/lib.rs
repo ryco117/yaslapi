10cf19b99e6787d0b7741ec62c1470d3d46bbe16
Ryan Andersen <ryco117@gmail.com>
2023-09-05 20:33:53 -0700

    Added the majority of remaining core API functions. Still some awkward calls and YASLX left to wrap

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,10 @@
 
 #![allow(clippy::must_use_candidate)]
 use num_derive::FromPrimitive;
-use std::{collections::HashSet, ffi::CString};
+use std::{
+    collections::HashSet,
+    ffi::{CStr, CString},
+};
 
 extern crate yaslapi_sys;
 
@@ -205,16 +208,179 @@ impl State {
         unsafe { yaslapi_sys::YASL_isuserptr(self.state) }
     }
 
-    /// Returns the type of the top of the stack.
-    pub fn peek_type(&self) -> Type {
-        unsafe { yaslapi_sys::YASL_peektype(self.state) }.into()
+    /// Checks if the object at index `n` from the top of the stack is a bool.
+    /// # Panics
+    /// The argument count `n` must be able to safely convert into a C unsigned integer.
+    pub fn is_n_bool(&mut self, n: usize) -> bool {
+        unsafe {
+            yaslapi_sys::YASL_isnbool(
+                self.state,
+                n.try_into()
+                    .expect("Index must be able to safely convert into a C unsigned integer."),
+            )
+        }
     }
-    /// Returns the type of the top of the stack as a string.
-    #[allow(clippy::missing_panics_doc)]
-    pub fn peek_type_name(&self) -> &str {
-        unsafe { std::ffi::CStr::from_ptr(yaslapi_sys::YASL_peektypename(self.state)) }
-            .to_str()
-            .unwrap()
+    /// Checks if the object at index `n` from the top of the stack is a float.
+    /// # Panics
+    /// The argument count `n` must be able to safely convert into a C unsigned integer.
+    pub fn is_n_float(&mut self, n: usize) -> bool {
+        unsafe {
+            yaslapi_sys::YASL_isnfloat(
+                self.state,
+                n.try_into()
+                    .expect("Index must be able to safely convert into a C unsigned integer."),
+            )
+        }
+    }
+    /// Checks if the object at index `n` from the top of the stack is an int.
+    /// # Panics
+    /// The argument count `n` must be able to safely convert into a C unsigned integer.
+    pub fn is_n_int(&mut self, n: usize) -> bool {
+        unsafe {
+            yaslapi_sys::YASL_isnint(
+                self.state,
+                n.try_into()
+                    .expect("Index must be able to safely convert into a C unsigned integer."),
+            )
+        }
+    }
+    /// Checks if the object at index `n` from the top of the stack is a list.
+    /// # Panics
+    /// The argument count `n` must be able to safely convert into a C unsigned integer.
+    pub fn is_n_list(&mut self, n: usize) -> bool {
+        unsafe {
+            yaslapi_sys::YASL_isnlist(
+                self.state,
+                n.try_into()
+                    .expect("Index must be able to safely convert into a C unsigned integer."),
+            )
+        }
+    }
+    /// Checks if the object at index `n` from the top of the stack is a string.
+    /// # Panics
+    /// The argument count `n` must be able to safely convert into a C unsigned integer.
+    pub fn is_n_str(&mut self, n: usize) -> bool {
+        unsafe {
+            yaslapi_sys::YASL_isnstr(
+                self.state,
+                n.try_into()
+                    .expect("Index must be able to safely convert into a C unsigned integer."),
+            )
+        }
+    }
+    /// Checks if the object at index `n` from the top of the stack is a table.
+    /// # Panics
+    /// The argument count `n` must be able to safely convert into a C unsigned integer.
+    pub fn is_n_table(&mut self, n: usize) -> bool {
+        unsafe {
+            yaslapi_sys::YASL_isntable(
+                self.state,
+                n.try_into()
+                    .expect("Index must be able to safely convert into a C unsigned integer."),
+            )
+        }
+    }
+    /// Checks if the object at index `n` from the top of the stack is `undef`.
+    /// # Panics
+    /// The argument count `n` must be able to safely convert into a C unsigned integer.
+    pub fn is_n_undef(&mut self, n: usize) -> bool {
+        unsafe {
+            yaslapi_sys::YASL_isnundef(
+                self.state,
+                n.try_into()
+                    .expect("Index must be able to safely convert into a C unsigned integer."),
+            )
+        }
+    }
+    /// Checks if the object at index `n` from the top of the stack is userdata of a given tag.
+    /// # Panics
+    /// The argument count `n` must be able to safely convert into a C unsigned integer.
+    pub fn is_n_userdata(&mut self, tag: &str, n: usize) -> bool {
+        unsafe {
+            yaslapi_sys::YASL_isnuserdata(
+                self.state,
+                tag.as_ptr().cast(),
+                n.try_into()
+                    .expect("Index must be able to safely convert into a C unsigned integer."),
+            )
+        }
+    }
+    /// Checks if the object at index `n` from the top of the stack is userpointer.
+    /// # Panics
+    /// The argument count `n` must be able to safely convert into a C unsigned integer.
+    pub fn is_n_userptr(&mut self, n: usize) -> bool {
+        unsafe {
+            yaslapi_sys::YASL_isnuserptr(
+                self.state,
+                n.try_into()
+                    .expect("Index must be able to safely convert into a C unsigned integer."),
+            )
+        }
+    }
+
+    /// Pops the top of the stack, then evaluates `len` on the popped value. The result is pushed to the stack.
+    pub fn len(&mut self) {
+        unsafe { yaslapi_sys::YASL_len(self.state) }
+    }
+
+    /// Indexes the list on top of the stack and pushes the result to the stack.
+    /// If `n` is negative it indexes from the end of the list.
+    /// # Panics
+    /// The argument count `n` must be able to safely convert into a 64-bit signed integer.
+    pub fn list_get(&mut self, n: isize) -> Error {
+        unsafe {
+            yaslapi_sys::YASL_listget(
+                self.state,
+                n.try_into()
+                    .expect("Index must be able to safely convert into a 64-bit signed integer."),
+            )
+        }
+        .into()
+    }
+
+    /// Pops the top of the stack and appends it to a list (which should be located directly below the top of the stack).
+    pub fn list_push(&mut self) -> Error {
+        unsafe { yaslapi_sys::YASL_listpush(self.state) }.into()
+    }
+
+    /// Loads the specified global from state and pushes it to the stack.
+    pub fn load_global(&mut self, name: &str) -> Error {
+        let name = CString::new(name).unwrap();
+        unsafe { yaslapi_sys::YASL_loadglobal(self.state, name.as_ptr()) }.into()
+    }
+
+    /// Loads a metatable by name.
+    pub fn load_metatable(&mut self, name: &str) -> Error {
+        let name = CString::new(name).unwrap();
+        unsafe { yaslapi_sys::YASL_loadmt(self.state, name.as_ptr()) }.into()
+    }
+
+    /// Returns the boolean value of the top of the stack, if it is a bool.
+    /// Otherwise, returns false.
+    pub fn peek_bool(&self) -> bool {
+        unsafe { yaslapi_sys::YASL_peekbool(self.state) }
+    }
+    /// Returns the C-string value of the top of the stack, if the top of the stack is a C-string.
+    /// Otherwise, returns `None`.
+    pub fn peek_cstr(&self) -> Option<&str> {
+        unsafe {
+            let ptr = yaslapi_sys::YASL_peekcstr(self.state);
+            if ptr.is_null() {
+                None
+            } else {
+                CStr::from_ptr(ptr).to_str().ok()
+            }
+        }
+    }
+    /// Returns the float value of the top of the stack, if the top of the stack is a float.
+    /// Otherwise, returns 0.0.
+    pub fn peek_float(&self) -> f64 {
+        unsafe { yaslapi_sys::YASL_peekfloat(self.state) }
+    }
+    /// Returns the int value of the top of the stack, if the top of the stack is an int.
+    /// Otherwise, returns 0.
+    pub fn peek_int(&self) -> i64 {
+        unsafe { yaslapi_sys::YASL_peekint(self.state) }
     }
     /// Returns the userdata value of the top of the stack, if the top of the stack is a userdata.
     pub fn peek_userdata(&self) -> Option<*mut ::std::os::raw::c_void> {
@@ -234,6 +400,90 @@ impl State {
             Some(ptr)
         }
     }
+    /// Returns the type of the top of the stack.
+    pub fn peek_type(&self) -> Type {
+        unsafe { yaslapi_sys::YASL_peektype(self.state) }.into()
+    }
+    /// Returns the type of the top of the stack as a string.
+    #[allow(clippy::missing_panics_doc)]
+    pub fn peek_type_name(&self) -> &str {
+        unsafe { CStr::from_ptr(yaslapi_sys::YASL_peektypename(self.state)) }
+            .to_str()
+            .expect("YASL internal error: YASL_peektypename returned invalid UTF-8")
+    }
+
+    /// Returns the bool value at index `n` from the top of the stack, if it is a boolean.
+    /// Otherwise returns false.
+    /// # Panics
+    /// The argument count `n` must be able to safely convert into a C unsigned integer.
+    pub fn peek_n_bool(&self, n: usize) -> bool {
+        unsafe {
+            yaslapi_sys::YASL_peeknbool(
+                self.state,
+                n.try_into()
+                    .expect("Index must be able to safely convert into a C unsigned integer."),
+            )
+        }
+    }
+    /// Returns the float value at index `n` from the top of the stack, if it is a float.
+    /// Otherwise returns 0.0.
+    /// # Panics
+    /// The argument count `n` must be able to safely convert into a C unsigned integer.
+    pub fn peek_n_float(&self, n: usize) -> f64 {
+        unsafe {
+            yaslapi_sys::YASL_peeknfloat(
+                self.state,
+                n.try_into()
+                    .expect("Index must be able to safely convert into a C unsigned integer."),
+            )
+        }
+    }
+    /// Returns the int value at index `n` from the top of the stack, if it is an int.
+    /// Otherwise returns 0.
+    /// # Panics
+    /// The argument count `n` must be able to safely convert into a C unsigned integer.
+    pub fn peek_n_int(&self, n: usize) -> i64 {
+        unsafe {
+            yaslapi_sys::YASL_peeknint(
+                self.state,
+                n.try_into()
+                    .expect("Index must be able to safely convert into a C unsigned integer."),
+            )
+        }
+    }
+    /// Returns the userdata value at index `n` from the top of the stack, if it is a userdata.
+    /// Otherwise returns `None`.
+    /// # Panics
+    /// The argument count `n` must be able to safely convert into a C unsigned integer.
+    pub fn peek_n_userdata(&self, n: usize) -> Option<*mut ::std::os::raw::c_void> {
+        let ptr = unsafe {
+            yaslapi_sys::YASL_peeknuserdata(
+                self.state,
+                n.try_into()
+                    .expect("Index must be able to safely convert into a C unsigned integer."),
+            )
+        };
+        if ptr.is_null() {
+            None
+        } else {
+            Some(ptr)
+        }
+    }
+    /// Returns the type of index `n` from the top of the stack as a string.
+    /// # Panics
+    /// The argument count `n` must be able to safely convert into a C unsigned integer.
+    pub fn peek_n_typename(&self, n: usize) -> &str {
+        unsafe {
+            CStr::from_ptr(yaslapi_sys::YASL_peekntypename(
+                self.state,
+                n.try_into()
+                    .expect("Index must be able to safely convert into a C unsigned integer."),
+            ))
+        }
+        .to_str()
+        .expect("YASL internal error: YASL_peektypename returned invalid UTF-8")
+    }
+
     /// TODO: Document.
     pub fn peek_vargs_count(&self) -> i64 {
         unsafe { yaslapi_sys::YASL_peekvargscount(self.state) }
@@ -243,12 +493,10 @@ impl State {
     pub fn pop(&mut self) {
         unsafe { yaslapi_sys::YASL_pop(self.state) }
     }
-
     /// Returns the bool value at the top of the stack, if the top of the stack is a boolean. Otherwise returns false. Removes the top element of the stack.
     pub fn pop_bool(&mut self) -> bool {
         unsafe { yaslapi_sys::YASL_popbool(self.state) }
     }
-
     /// Returns the str value (nul-terminated) of the top of the stack, if the top of the stack is a str.
     pub fn pop_cstr(&mut self) -> Option<*mut ::std::os::raw::c_char> {
         let ptr = unsafe { yaslapi_sys::YASL_popcstr(self.state) };
@@ -284,6 +532,8 @@ impl State {
             Some(ptr)
         }
     }
+
+    // TODO: Rust doesn't really support variadic argument lists; more reading required.
     // Prints a runtime error. @param S the YASL_State in which the error occurred. @param fmt a format string, taking the same parameters as printf.
     // pub fn print_err(S: *mut YASL_State, fmt: *const ::std::os::raw::c_char, ...) {
     //     unsafe { yaslapi_sys::YASL_print_err(S, fmt) }
@@ -305,8 +555,8 @@ impl State {
     pub fn push_int(&mut self, i: i64) {
         unsafe { yaslapi_sys::YASL_pushint(self.state, i) }
     }
-    /// Pushes a length-specified string onto the stack. YASL makes a copy of the given string, and manages the memory for it. The string may have embedded zeroes.
-    pub fn push_lstr(&mut self, string: &[i8]) {
+    /// Pushes a bytes-buffer onto the stack. YASL makes a copy of the given data, and manages the memory for it. The buffer may have embedded zeroes.
+    pub fn push_buffer(&mut self, string: &[i8]) {
         unsafe { yaslapi_sys::YASL_pushlstr(self.state, string.as_ptr(), string.len()) }
     }
     /// Pushes an empty list onto the stack.
@@ -314,7 +564,7 @@ impl State {
         unsafe { yaslapi_sys::YASL_pushlist(self.state) }
     }
     /// Pushes a nul-terminated string onto the stack. This memory will not be managed by YASL and must outlive the state.
-    pub fn push_lit(&mut self, string: &'static str) {
+    pub fn push_literal(&mut self, string: &'static str) {
         unsafe { yaslapi_sys::YASL_pushlit(self.state, string.as_ptr().cast()) }
     }
     /// Pushes an empty table onto the stack.
@@ -344,7 +594,7 @@ impl State {
     pub unsafe fn push_userptr(&mut self, userpointer: *mut ::std::os::raw::c_void) {
         unsafe { yaslapi_sys::YASL_pushuserptr(self.state, userpointer) }
     }
-    /// Pushes a nul-terminated string onto the stack. YASL makes a copy of the given string, and manages the memory for it. The string may not have embedded 0s; it is assumed to end at the first 0.
+    /// Pushes a nul-terminated string onto the stack. YASL makes a copy of the given string, and manages the memory for it.
     pub fn push_zstr(&mut self, cstring: &CString) {
         unsafe { yaslapi_sys::YASL_pushzstr(self.state, cstring.as_ptr()) }
     }
@@ -366,18 +616,48 @@ impl State {
     /// parameters it will be called with. The left-most parameter is placed directly above
     /// the function, the right-most paramter at the top of the stack.
     /// # Panics
-    /// The argument count `n` must be able to safely convert into a non-negative 32-bit signed integer.
+    /// The argument count `n` must be able to safely convert into a non-negative C signed integer.
     pub fn function_call(&mut self, n: usize) -> Error {
-        unsafe { yaslapi_sys::YASL_functioncall(
-            self.state,
-            n.try_into().expect("The input argument count cannout be safely converted to a 32-bit signed integer.")
-        ) }.into()
+        unsafe {
+            yaslapi_sys::YASL_functioncall(
+                self.state,
+                n.try_into().expect(
+                    "The input argument count cannout be safely converted to a C signed integer.",
+                ),
+            )
+        }
+        .into()
     }
 
+    /// Recreate the state machine from the given script path.
+    pub fn reset_from_script(&mut self, script_location: &str) -> Error {
+        let script_location = CString::new(script_location).unwrap();
+        unsafe { yaslapi_sys::YASL_resetstate(self.state, script_location.as_ptr()) }.into()
+    }
     /// Recreate the state machine from the given source code.
     pub fn reset_from_source(&mut self, source: &str) -> Error {
         unsafe { yaslapi_sys::YASL_resetstate_bb(self.state, source.as_ptr().cast(), source.len()) }
             .into()
+    }
+
+    /// Pops the top of the YASL stack and stores it in the given global.
+    pub fn set_global(&mut self, name: &str) -> Error {
+        let name = CString::new(name).unwrap();
+        unsafe { yaslapi_sys::YASL_setglobal(self.state, name.as_ptr()) }.into()
+    }
+
+    /// Iterates over a table. The top-most item of the stack should be the previous index in
+    /// the table, followed by the table itself. The index is popped, and then if there are
+    /// more elements in the table, the next index and value are pushed. No values are pushed
+    /// if we are already at the end of the table.
+    pub fn table_next(&mut self) -> bool {
+        unsafe { yaslapi_sys::YASL_tablenext(self.state) }
+    }
+
+    /// Inserts a key-value pair into the table. The top-most items are the value, then key,
+    /// then table. The key and value are popped from the stack.
+    pub fn table_set(&mut self) -> Error {
+        unsafe { yaslapi_sys::YASL_tableset(self.state) }.into()
     }
 }
 
